68a1d354b5bdb402804ea362f067b8da1db5cfc7
myoamorim <34634335+myoamorim@users.noreply.github.com>
2019-09-29 19:25:41 -0300

    Add files via upload

---
+++ GerenciandoADDS.ps1
@@ -0,0 +1,18 @@
+﻿(Get-Command -Module ActiveDirectory).count
+
+Get-Command New-ad*
+Get-Command Get-ad*
+
+#Para adicionar usuáro
+New-ADUser Curso.POSH
+
+Get-ADUSer mayconcarlos -Properties LastLogonDate
+Get-ADUSer -Filter * | where {$_.enabled -eq $false}
+
+#Para exportar CSV
+Get-ADUSer -Filter * -Properties * | Export-csv C:\users\MayconCarlos\listauser.csv
+Get-ADUSer -Filter * -Properties * | select -Property GivenName, DisplayName
+
+
+# Para importar CSV 
+Import-Csv C:\users\MayconCarlos\listauser.csv
